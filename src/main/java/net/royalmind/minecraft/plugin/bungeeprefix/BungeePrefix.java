1722a639162e0d1a9a2b30e4eaad22285ca1fb74
Dali <oscardali370@gmail.com>
2022-01-05 18:44:54 -0600

    Initial commit

---
+++ src/main/java/net/royalmind/minecraft/plugin/bungeeprefix/BungeePrefix.java
@@ -0,0 +1,16 @@
+package net.royalmind.minecraft.plugin.bungeeprefix;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BungeePrefix extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
